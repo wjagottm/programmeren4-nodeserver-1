ef309c0969c5d250096efbd75d986d1373278fbb
Robin Schellius <r.schellius@avans.nl>
2018-04-19 15:29:51 +0200

    Intial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+let http = require('http');
+
+http.createServer(function (request, response){
+	console.log('Er was een request');
+	response.writeHead(200, {'Content-Type': 'application/json'});
+	let result = {
+		firstname: 'Robin',
+		lastname: "Schellius"
+	};
+	response.write(JSON.stringify(result));
+	response.end();
+}).listen(3000);
+
+console.log('De server luistert op port 3000');
