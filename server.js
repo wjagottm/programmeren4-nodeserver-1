f781d105b8a46fc2300623295c5fc574c536954f
wjagottm <wjagottm@avans.nl>
2018-04-24 10:04:09 +0200

    Express toegevoegd

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 //
 // server.js
 //
-let http = require('http');
+const express = require('express');
+const app = express();
 
 const port = process.env.PORT || 3000;
 
-http.createServer(function (request, response){
-	console.log('Er was een request');
-	response.writeHead(200, {'Content-Type': 'application/json'});
-	let result = {
-		firstname: 'Robin',
-		lastname: "Schellius"
-	};
-	response.write(JSON.stringify(result));
-	response.end();
-}).listen(port);
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
 
-console.log('De server luistert op port ' + port);
+app.listen(port, () => {
+	console.log('Example app listening on port ' + port + '!')
+});
